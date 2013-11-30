1373568f6c5dcbf6ed308ca68328523232f10e67
Flavio <perezflavio@gmail.com>
2013-11-30 22:27:48 +0100

    IE Alert

---
+++ static/js/gifGiftIEALERT.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+   
+    alert("En/ We are sorry, but our calendar does not work well on Internet Explorer. Please use Chrome, Firefox or Safari\nFr/ Nous sommes desoles mais notre calendrier ne marche pas bien sur Internet Explorer. Utilisez Chrome, Firefox ou Safari s'il vous plait");
+    
+});
